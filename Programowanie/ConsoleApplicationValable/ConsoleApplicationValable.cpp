b345b93ccd3e1b055fc3604fecd41f1b2cb548e8
Sala213 <Sala213@ezse.pl>
2025-09-23 08:33:13 +0200

    Hii

--- Programowanie/ConsoleApplicationValable/ConsoleApplicationValable.cpp
+++ Programowanie/ConsoleApplicationValable/ConsoleApplicationValable.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 
+//Napisz program który wczyta liczbê od u¿ytkownika i j¹ wyœwietli na konsoli
 int main()
 {
-    std::cout << "Hello World!\n";
+    //wczytanie liczby
+        //deklaracja zmiennych          1
+        //pobranie liczby
+            //informacja co chcemy      2
+            //oczekiwanie na liczbê     3
+
+    //wyœwietlenie na konsoli           4
+
+    //1
+    int numberFromUser;
 }
 
+
 /*
-Zmiena - pewien obszar w pamiêci operacyjnej, w której mo¿na w danej chwili przechowywaæ tylko jedn¹ dan¹.
 
+Zmienna - pewien obszar w pamiêci operacyjnej, w której mo¿na
+w danej chwili przechowaæ tylko jedn¹ dan¹.
 
-Instrukcja deklaracji zmienej
-typ_zmienej
+Instrukcja daklaracji zmiennej:
+typ_zmienej nazwaZmiennej;
 
-Typ zmienej - wielkoœæ obszaru pamiêci, interptracji ci¹gu bitów
+Typ zmiennej - wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów
 
-int - 4 bajtowa liczba ca³kowita ze znakiem <-2 147 483 648, 2 147 483 648>.
+int - 4 bajtowa liczba ca³kowita ze znakiem <-2 147 483 648, 2 147 483 647>
 
+Nazwa zmiennej - nazwa obszaru w pamiêci, identyfikator
+Warunki niezbêdne:
+* dozwolone znaki:
+    - alfabet angielski aA-zZ
+    - cyfry arabskie 0-9
+    - podkreœlenie (pod³oga) _
+* pierwszym znakiem nie mo¿e byæ cyfra
+* unikalny w swoim zakresie widocznoœci
+* nie mo¿e to byæ s³owo kluczowe (zarezerwowane) danego jêzyka
 
+Warunki programistów:
+* nazwa zmiennej powinna oddawaæ charakter przechowywanych danych
+* jeœli wiele s³ów to w miejscu spacji podkreœlenie lub zaczynaj¹c od drugiego
+  s³owa piszemy je z du¿ej litery
+* piszemy po angielsku
 */
-
