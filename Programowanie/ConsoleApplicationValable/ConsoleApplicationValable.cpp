9498980295235d317eb9970cd123cdb170e02a34
Sala213 <Sala213@ezse.pl>
2025-10-07 09:55:59 +0200

    hiii

--- Programowanie/ConsoleApplicationValable/ConsoleApplicationValable.cpp
+++ Programowanie/ConsoleApplicationValable/ConsoleApplicationValable.cpp
@@ -1,17 +1,7 @@
 ﻿#include <iostream>
 
-/*
-*.
-*. Program obliczający objętość stożka.
-*. Program obliczający pole koła.
-*. Program obliczający wartość wyrażenia a^2 + b^2
-*. Program obliczający pole trójkąta o podstawie b i wysokości h
-*. Program obliczający objętość kuli o promieniu r
-*. Program obliczający pole trapezu o podstawach a i b oraz wysokości h
-*. Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
-*. Stwórz program, który pobiera od użytkownika długość w metrach i przelicza ją na centymetry i milimetry, wyświetlając wynik.
-*. Napisz program, który konwertuje wprowadzoną kwotę w jednej walucie na inną, korzystając z określonego kursu wymiany.
-*/
+
+// Napisz program, który konwertuje wprowadzoną kwotę w jednej walucie na inną, korzystając z określonego kursu wymiany.
 
 //Napisz program który wczyta liczbę od użytkownika i ją wyświetli na konsoli
 void task1()
@@ -32,7 +22,6 @@ void task1()
 	//4
 	std::cout << "Podałeś " << numberFromUser << "\n";
 }
-
 //Program obliczający średnią arytmetyczną dwóch liczb.
 void task2()
 {
@@ -66,8 +55,6 @@ void task3()
 
 	std::cout << "Pole prostokąta wynosi" << area << "\n";
 }
-
-
 //Program obliczający pole stożka.
 void task4()
 {
@@ -79,22 +66,92 @@ void task4()
 	std::cin >> h;
 	std::cout << "Objetosc stozka= " << (1.0f / 3.0f) * 3.14 * r * r * h << std::endl;
 }
-////Program obliczający pole koła
-//void task4()
-//{
-//	float p, h;
-//	std::cout << "======[Pole koła]======\n";
-//	std::cout << " Podaj promien= ";
-//	std::cin >> p;
-//	std::cout << "Podaj srednicę= ";
-//	std::cin >> h;
-//	std::cout << "Pole kola= " <<p * h << std::endl;
-//}
+//Program obliczający pole koła
+void task5()
+{
+	float r;
+	std::cout << "======[Pole koła]======\n";
+	std::cout << " Podaj promien= ";
+	std::cin >> r;
+	std::cout << "Pole kola= " << 3.14 * r * r << std::endl;
+}
+//Program obliczający wartość wyrażenia a^2 + b^2
+void task6()
+{
+	float a, b;
+	std::cout << "Podaj liczbe a ktora będzie pomnozona przez kwadrat: ";
+	std::cin >> a;
+	std::cout << "Podaj liczbe b ktora bedzie pomnozona przez kwadrat: ";
+	std::cin >> b;
+	std::cout << "Wynik dodawania a i b gdzie oby dwie liczby sa pomnozone przez kwadrat:" << a * a + b * b << std::endl;
+}
+//Program obliczający pole trójkąta o podstawie b i wysokości h
+void task7()
+{
+	float b, h;
+	std::cout << "Podaj podstawę: ";
+	std::cin >> b;
+	std::cout << "Podaj wysokość: ";
+	std::cin >> h;
+	std::cout << "Pole trójkąta wynosi: " << (b * h) / 2 << std::endl;
+}
+//Program obliczający objętość kuli o promieniu r
+void task8()
+{
+	float r;
+	std::cout << "Podaj promień: ";
+	std::cin >> r;
+	std::cout << "Objętość kuli o promieniu r wynosi:" << (4.0 / 3.0) * 3.14 * r * r * r << std::endl;
+}
+//Program obliczający pole trapezu o podstawach a i b oraz wysokości h
+void task9()
+{
+	float a, b, h;
+	std::cout << "Podaj długość podstawy a: ";
+	std::cin >> a;
+	std::cout << "Podaj długość podstawy b: ";
+	std::cin >> b;
+	std::cout << "Podaj wysokość: ";
+	std::cin >> h;
+	std::cout << "Pole trapezu wynosi: " << (a + b) * h /2 << std::endl;
+}
+//Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+void task10()
+{
+	double x1, x2, x3;
+	double w1, w2, w3;
+
+	std::cout << "Podaj pierwsza liczbe: ";
+	std::cin >> x1;
+	std::cout << "Podaj wage pierwszej liczby: ";
+	std::cin >> w1;
+
+	std::cout << "Podaj drugą liczbe: ";
+	std::cin >> x2;
+	std::cout << "Podaj wage drugiej liczby: ";
+	std::cin >> w2;
+
+	std::cout << "Podaj trzecią liczbe: ";
+	std::cin >> x3;
+	std::cout << "Podaj wage trzeciej liczby: ";
+	std::cin >> w3;
+
+	std::cout << "Waga podanych licz wynosi: " << (x1 * w1 + x2 * w2 + x3 * w3) /(w1 + w2 + w3) << std::endl;
+}
+//Stwórz program, który pobiera od użytkownika długość w metrach i przelicza ją na centymetry i milimetry, wyświetlając wynik.
+void task11()
+{
+	float metry;
+	std::cout << "Podaj ilość metrów:";
+	std::cin >> metry;
+	std::cout << " Zmiana metrów na centymetry " << metry*100 << " Zmiana metrów na milimetry " << metry*1000 << std::endl;
+}
 
 
 int main()
 {
-	task4();
+	setlocale(LC_CTYPE, "polish");
+	task11();
 }
 
 
