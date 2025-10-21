53ae9d8b2588ecd1336e2e677aa5ff2c2ae68680
Sala213 <Sala213@ezse.pl>
2025-10-21 09:53:14 +0200

    hi

--- Programowanie/InstructionIFConsoleApplication/InstructionIFConsoleApplication.cpp
+++ Programowanie/InstructionIFConsoleApplication/InstructionIFConsoleApplication.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 
+/*
+Operatry warunkowe:
+>		- wiêkszy
+<		- mniejszy
+>=		- wiekszy b¹dŸ równy
+<=		- mniejszy b¹dŸ równy
+==		- równy
+!=		- ró¿ny
+
+*/
+
 //Napisz program, który wyœwietli informacje czy liczba jest dodatnia czy nie.
 void task1()
+{
+	int number;
+	std::cout << "Podaj liczbê ca³kowit¹:\n";
+	std::cin >> number;
+
+	if (number > 0)
+		std::cout << "Podana liczba jest dodatnia\n";
+
+	if (number < 0)
+		std::cout << "Podana liczba jest ujemna\n";
+
+	if (number == 0)
+		std::cout << "Podana liczba nie jest dodatnia ani ujemna\n";
+
+	std::cout << "Koniec programu\n";
+}
+
+//Napisz program, który wyœwietli informacje czy liczba jest parzysta czy nieparzysta.
+void task2()
 {
 	int number;
 	std::cout << "Podaj liczbê ca³kowit¹:\n";
@@ -10,22 +40,42 @@ void task1()
 	int reminderOfDivision;
 	reminderOfDivision = number % 2;
 
-	if (reminderOfDivision > 0)
+	if (reminderOfDivision == 0)
 		std::cout << "Podana liczba jest parzysta\n";
-	
-	if (reminderOfDivision < 0)
-		std::cout << "Podana liczba jest nie parzysta\n";
 
+	if (reminderOfDivision != 0) // == 1 
+		std::cout << "Podana liczba jest nieparzysta\n";
 
-
-
-	std::cout << "Koniec programu\n";
+	//--------------------------------------------------------
+	if (reminderOfDivision == 0)
+		std::cout << "Podana liczba jest parzysta\n";
+	else
+		std::cout << "Podana liczba jest nieparzysta\n";
 }
 
+//Napisz program, który wyœwietli informacje czy liczba jest z zakresu <1 ; 10).
+void task3()
+{
+	int number;
+	std::cout << "Podaj liczbê ca³kowit¹:\n";
+	std::cin >> number;
+
+	if (number >= 1)
+	{
+		if (number < 10)
+			std::cout << "Liczba jest w zakresie <1;10)\n";
+		else
+			std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	}
+	else
+	{
+		std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	}
+}
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 
-	task1();
+	task3();
 }
